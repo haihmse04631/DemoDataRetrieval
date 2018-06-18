ddf162cf86375ebbdc3ef8d4238ef72d823e6fbd
hai.hoangminh <hai.hoangminh@vietis.com.vn>
2018-06-18 17:15:18 +0700

    modify code

--- common_util/utility.js
+++ common_util/utility.js
@@ -1,33 +1,64 @@
 const IOTA = require('iota.lib.js')
 const iota = new IOTA({ provider: 'http://localhost:14700' })
 
-function utility(){
+function utility() {
 
 }
 
-utility.prototype.getInforTransaction = function(tail){
-    iota.api.getBundle(tail, function(error, data){
-        if(error){
-            console.log(error);
-        }else{
-            console.log(data);
+/**
+ * cut '99999' from signatureMessageFragment 
+ * 
+ * @method formatMessage
+ * @param {String} message
+ * @returns {String} new string message
+ */
+
+utility.prototype.formatMessage = function (message) {
+    var index = message.length - 1;
+    var isFirstIndexOf9 = true;
+    for (let i = 0; i < message.length; i++) {
+        if (message[i] == '9') {
+            if (isFirstIndexOf9) {
+                index = i
+                isFirstIndexOf9 = false
+            }
+        } else {
+            index = message.length - 1
+            isFirstIndexOf9 = true
         }
-    })
+    }
+    let stMessage = message.substring(0, index)
+    return stMessage
 }
 
-utility.prototype.getAccountData = function(seed){
-    iota.api.getAccountData(seed, function(error, data){
-        if(error){
-            console.log(error);
-        }else{
-            return data
+
+/**
+ * cut '99999' from signatureMessageFragment and decode 
+ * 
+ * @method formatMessage
+ * @param {String} message
+ * @returns {String} new string message
+ */
+utility.prototype.formatAndDecodeMessage = function (message) {
+    var index = message.length;
+    var isFirstIndexOf9 = true;
+    for (let i = 0; i < message.length; i++) {
+        if (message[i] == '9') {
+            if (isFirstIndexOf9) {
+                index = i
+                isFirstIndexOf9 = false
+            }
+        } else {
+            index = message.length
+            isFirstIndexOf9 = true
         }
-    })
+    }
+    let stMessage = message.substring(0, index)
+    return iota.utils.fromTrytes(stMessage)
 }
 
-utility.prototype.toMessage = function(){
-    
-}
+
+
 
 module.exports = utility
 
--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const IOTA = require('iota.lib.js')
 const UTIL = require('./common_util/utility.js')
 const util = new UTIL()
 const iota = new IOTA({ provider: 'http://localhost:14700' })
-
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 // iota.api.getNodeInfo(function(error, success) {
 //     if (error) {
@@ -11,7 +15,65 @@ const iota = new IOTA({ provider: 'http://localhost:14700' })
 //         console.log(success);
 //     }
 // })
+
 var seed = `VERTAWNTFULJCUNRZXPVBHQKBTSOOPPPNRBQ9AUYDRDFPVIJBIOBLNLNS9GGVUHXIJCHTCVOQQGVRY9IH`
-var st = `VERTAWNTFULJCUNRZXPVBHQKBTSOOPPPNRBQ9AUYDRDFPVIJBIOBLNLNS9GGVUHXIJCHTCVOQQGVRY9IH`
-var accountData = util.getAccountData(seed)
-console.log(accountData);
+var seed2 = `VERTAWNTFULJCUNRZXPVBHQKBTSEOPPPNRBQ9AUYDRDFPVIJBIOBLNTNS9GGVUHXIJCHTCVOQQGVRY9IH`
+var address = `OJXBFNYDMJITDUNA9AINIQLVQCGQCGUVNNTSRJYZDZMGGZANBXT9GDVGGADOCIRYM9HYBRSNVV9QAKDQAWLVQK9MAW`
+var st = `ADAVAS99ASDWV9DAsd9AADASd9999999999999999999999999999999999999999999999999ACAs9999999`
+// var accountData = util.getAccountData(seed)
+// console.log(accountData);
+
+// iota.api.getAccountData(seed, function(error, data){
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log(data);
+        
+//     }
+// })
+
+function getAllTranfer(address){
+    
+}
+var message = iota.utils.toTrytes(`Hello World!!!`)
+var message3 = iota.utils.toTrytes(`Hello World33333`)
+const tranfers = [
+    {
+        value: 5,
+        address: address,
+        message: message
+    },
+    {
+        value: 7,
+        address: address,
+        message: message3
+    },
+    {
+        value: 0,
+        address: address,
+        message: message3
+    }
+]
+
+const tranfers2 = [
+    {
+        value: 0,
+        address: address,
+        message: message
+    },
+]
+
+function sendData(seed, tranfers){
+    iota.api.sendTransfer(seed, 3, 9,tranfers,function(error, success){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(success);
+        }
+    })
+}
+
+sendData(seed, tranfers2)
+
+
+
